8ceae96b7225b53f7dd4beb7802c939b2a4398fd
Fatma Ceren Çil <fatmaceren2002@gmail.com>
2025-04-13 23:41:57 +0300

    Add division operation

--- calculator.py
+++ calculator.py
@@ -1,4 +1,10 @@
 def add(x, y):
     return x + y
 
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero!"
+    return x / y
+
 print("5 + 3 =", add(5, 3))
+print("10 / 2 =", divide(10, 2))
